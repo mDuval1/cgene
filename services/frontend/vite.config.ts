589efdd3b4c1d4eb2d5e2c66bcadb62d46ae2f44
mDuval1 <maxime.duval@dnascript.co>
2023-02-11 23:04:08 +0100

    feat: initialize frontend service

---
+++ services/frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
+    }),
+  ],
+});
